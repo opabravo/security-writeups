a80032ecdee27fe67a669dd069119542e1109683
FateWalker <21262207+opabravo@users.noreply.github.com>
2023-01-26 14:11:41 +0800

    Enhance Script

--- pdf_enc.py
+++ pdf_enc.py
@@ -7,33 +7,35 @@ import glob
 import re
 
 
-def get_pdf_files(pdf_dir: str) -> list:
+OUTPUT_MACHINE_PATH = Path.cwd() / "htb" / "Machines"
+
+
+def get_pdf_files(pdf_dir: str) -> GeneratorExit:
     """Get a list of all PDF files in the directory"""
     # Get a list of all PDF files in the current directory
-    root_path = Path.cwd()
-    source_pdf_path = root_path / pdf_dir
-    return glob.glob(f"{source_pdf_path}/*.pdf")
+    source_pdf_path = Path.cwd() / pdf_dir
+    yield from glob.glob(f"{source_pdf_path}/*.pdf")
 
-def get_flag_from_pdf(page: PageObject) -> list:
+
+def get_flag_from_page(page: PageObject) -> list:
     """Extract the root flag from the PDF file"""
     text = page.extract_text()
     return re.findall(r"([a-fA-F0-9]{32})", text)
 
 
-def get_pdf_output_path(pdf_file) -> str:
+def get_pdf_output_path(pdf_file) -> Path:
     original_name = Path(pdf_file).name
     output_name = original_name.split("-")[1].strip()
-    output_pdf_path = Path.cwd() / "htb" / "Machines"
-    return f"{output_pdf_path}/{output_name}"
+    return OUTPUT_MACHINE_PATH / output_name
 
 
-def encrypt_pdf(pdf_file):
+def encrypt_pdf(pdf_file: str, output_path: str):
     reader = PdfReader(pdf_file)
     writer = PdfWriter()
 
     # Loop over each page in the PDF file
     for page in reader.pages:
-        if flags := get_flag_from_pdf(page):
+        if flags := get_flag_from_page(page):
             flag = flags[0]
         # print(f"[*] Found flag: {flag}")
         writer.add_page(page)
@@ -41,19 +43,20 @@ def encrypt_pdf(pdf_file):
     # Encrypt the PDF file
     print(f"[*] Encrypting {pdf_file} ... | {flag}")
     writer.encrypt(flag)
-    output_pdf_path = get_pdf_output_path(pdf_file)
 
     # Save the new PDF to a file
-    with open(output_pdf_path, "wb") as f:
+    with open(output_path, "wb") as f:
         writer.write(f)
-    print(f"[+] Encrypted PDF saved to {output_pdf_path}\n")
+    print(f"[+] Encrypted PDF saved to {output_path}\n")
 
 
 if __name__ == "__main__":
-    pdf_files = get_pdf_files("htb-unencrypted")
-
     # Loop over each PDF file
-    for pdf_file in pdf_files:
+    for pdf_file in get_pdf_files("htb-unencrypted"):
+        output_pdf_path = get_pdf_output_path(pdf_file)
+        if output_pdf_path.exists():
+            print(f"[!] {output_pdf_path} already exists. Skipping...")
+            continue
         encrypt_pdf(pdf_file)
 
     input("Press any key to exit...")
